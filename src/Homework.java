d542790d53a223442940d3f89a866b47dea6effe
Roman Khvalbota <khvalbota.10@gmail.com>
2020-11-02 14:51:14 +0200

    Changed homework 3.2 by adding exception handling

--- src/Homework.java
+++ src/Homework.java
@@ -4,16 +4,19 @@ import java.io.InputStreamReader;
 
 public class Homework {
     public static void main(String[] args) {
-        String name;
-        String address;
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            System.out.println("What is your name?");
-            name = br.readLine();
-            System.out.println("What is your address?");
-            address = br.readLine();
-            System.out.println("Your name is: "+name+"\nYour address is: "+address);
-        } catch(IOException exc){
-            System.out.println("Input-Output Error" + exc);
+        double radius, perimeter, area;
+
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+                System.out.println("Please enter the radius of a circle in centimeters:");
+                radius = Double.parseDouble(br.readLine());
+                perimeter = radius * 2 * Math.PI;
+                System.out.printf("The perimeter of a circle is: %.2f cm\n", perimeter);
+                area = radius * radius * Math.PI;
+                System.out.printf("The area of the circle is: %.2f cm\n", area);
+            }catch (IOException exc){
+                System.out.println("Input-Output exception");
+            }catch (NumberFormatException number){
+                System.out.println("You entered a symbol, not a number");
         }
     }
 }
