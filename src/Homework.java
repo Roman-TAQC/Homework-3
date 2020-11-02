7f686c704d2b720bc5f922838a855ee0ac49cc23
Roman Khvalbota <khvalbota.10@gmail.com>
2020-11-02 17:41:07 +0200

    Changed homework 3.5 by adding exception handling

--- src/Homework.java
+++ src/Homework.java
@@ -4,33 +4,69 @@ import java.io.InputStreamReader;
 
 public class Homework {
     public static void main(String[] args){
-        int a, b, c, x, y;
-        boolean flag = false;
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            System.out.println("Enter value of a:");
-            a = Integer.parseInt(br.readLine());
-            System.out.println("Enter value of b:");
-            b = Integer.parseInt(br.readLine());
-            System.out.println("Enter value of c:");
-            c = Integer.parseInt(br.readLine());
-            System.out.println("Enter value of x:");
-            x = Integer.parseInt(br.readLine());
-            System.out.println("Enter value of y:");
-            y = Integer.parseInt(br.readLine());
-            if(a<=x && b<=y || a<=y && b<=x)
-                flag = true;
-            if(a<=x && c<=y || a<=y && c<=x)
-                flag = true;
-            if(b<=x && c<=y || b<=y && c<=x)
-                flag = true;
-            if (flag)
-                System.out.println("GREAT! The brick is small enough to fit in!");
-            else
-                System.out.println("Sorry the brick is too big, not today :(");
-        }catch (IOException exc){
+        String str = null;
+        int num = -1;
+
+        //get a string and parse it to number
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            System.out.println("Enter a number:");
+            do{
+                try{
+                    str = br.readLine();
+                    num = Integer.parseInt(str);
+                }catch (IOException exc) {
+                    System.out.println("Input-Output exception");
+                }catch (NumberFormatException number){
+                    System.out.println("It's not a valid input. Enter number in format \"245\".");
+                }
+            }while(str == null || num < 1);
+        }catch (IOException exception){
             System.out.println("Input-Output exception");
-        }catch (NumberFormatException exc){
-            System.out.println("Wrong format");
         }
+
+        char [] arr;
+
+        // check if the digit 3 is a part of n-squared
+        num *= num;
+        String s = Integer.toString(num);
+        arr = s.toCharArray();
+        boolean flag = false;
+        for (char c: arr){
+            if (c == '3'){
+                flag = true;
+                break;
+            }
+        }
+        if (flag)
+            System.out.println("Digit 3 is a part of "+str+"-squared");
+        else
+            System.out.println("Digit 3 is not a part of "+str+"-squared");
+
+        // print the numerals in reverse order
+        arr = str.toCharArray();
+        int [] i = new int[str.length()];
+        for (int j = arr.length-1, k = 0; j>=0; j--, k++)
+            i[k] = Character.getNumericValue(arr[j]);
+        for (int j: i)
+            System.out.print(j);
+        System.out.println();
+
+        // swap first and last numerals and print altered number
+        arr = str.toCharArray();
+        char first = arr[0];
+        char last = arr[arr.length-1];
+        arr[0] = last;
+        arr[arr.length-1] = first;
+        String reverse = new String(arr);
+        System.out.println(reverse);
+
+        // add 1's at the beginning and at the end of a numeral
+        char[] alt = new char[arr.length+1];
+        alt [0] = '1';
+        alt [alt.length-1] = '1';
+        for (int c = 0; c < alt.length-2; c++)
+            alt[c+1] = arr[c];
+        String one = new String(alt);
+        System.out.println(one);
     }
 }
